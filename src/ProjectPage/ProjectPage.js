93e241cb3fc6ed30792e8260200e808befd80982
Yining Shi <ys2189@nyu.edu>
2017-04-30 14:34:05 -0400

    fixed icon bug

--- src/ProjectPage/ProjectPage.js
+++ src/ProjectPage/ProjectPage.js
@@ -9,7 +9,7 @@ import s from './ProjectPage.css'
 import {connect} from 'react-redux'
 import {getProject, getProjects} from '../redux/actions/projects'
 import {selectProject} from '../redux/selectors/projects'
-import {isEmpty} from 'lodash'
+import {isEmpty, get} from 'lodash'
 import {Link} from 'react-router-dom'
 import logo from '../img/itp-logo-blue.svg'
 import Slider from 'react-slick'
@@ -67,6 +67,7 @@ class ProjectPage extends React.Component {
 
 
   render() {
+    const { project } = this.props
     return (
       <div className={s.content}>
 
@@ -79,31 +80,31 @@ class ProjectPage extends React.Component {
         <div className={s.greyHeader}>
 
           <div className={`${s.topicIcon}`}>
-            <img src={require(`../img/${(this.props.project.topics.length > 0) ? this.props.project.topics[0].slug : 'education'} copy.svg`)} alt={"topic-icon"} style={iconStyles} color="#292755" />
+            <img src={require(`../img/${(get(project, 'topics.0.slug') !== '') ? get(project, 'topics.0.slug') : 'education'} copy.svg`)} alt={"topic-icon"} style={iconStyles} color="#292755" />
           </div>
-            <h3 className={s.projectTitle} dangerouslySetInnerHTML={{ __html: this.props.project.project_title}}/>
-          <h4 className={s.studentName}>{this.props.project.student_name}</h4>
+            <h3 className={s.projectTitle} dangerouslySetInnerHTML={{ __html: project.project_title}}/>
+          <h4 className={s.studentName}>{project.student_name}</h4>
 
           <hr className={s.separator}/>
 
           <div className={s.tagsTopicsAdvisor}>
-            <p>Category: {this.props.project.topics.map((topic, i) =>
+            <p>Category: {project.topics.map((topic, i) =>
                 <span key={i}>{topic.name}
-                {i === this.props.project.topics.length - 1 ? '' : ', '}
+                {i === project.topics.length - 1 ? '' : ', '}
                 </span>
               )}
             </p>
-            <p>Advisor: {this.props.project.advisor_name}</p>
+            <p>Advisor: {project.advisor_name}</p>
           </div>
         </div>
 
         <div className={s.bodyContainer}>
 
-          <p className={s.shortDescription} dangerouslySetInnerHTML={{ __html: this.props.project.short_description}}></p>
+          <p className={s.shortDescription} dangerouslySetInnerHTML={{ __html: project.short_description}}></p>
           <br/>
 
           {
-            this.props.project.slide_show.length > 0 &&
+            project.slide_show.length > 0 &&
             <Slider
               dots={true}
               infinite={true}
@@ -115,7 +116,7 @@ class ProjectPage extends React.Component {
               slidesToShow={1}
               slidesToScroll={1}
             >
-              {this.props.project.slide_show.map(slide =>
+              {project.slide_show.map(slide =>
                 <div>
                   <img rel="external" className={s.sliderImage} src={slide.src}/>
                   <p className={s.caption}>{slide.caption}</p>
@@ -130,11 +131,11 @@ class ProjectPage extends React.Component {
           </div>
 
           <div className={s.longDescription}>
-            <p className={s.bodyCopy} dangerouslySetInnerHTML={{ __html: this.props.project.description }} />
-            <p className={s.bodyCopy} dangerouslySetInnerHTML={{ __html: this.props.project.further_reading }} />
+            <p className={s.bodyCopy} dangerouslySetInnerHTML={{ __html: project.description }} />
+            <p className={s.bodyCopy} dangerouslySetInnerHTML={{ __html: project.further_reading }} />
 
-            <p className={s.addLinks}><a target="_blank" rel="external" href={this.props.project.video_presentation_url}>Video Presentation</a></p>
-            {this.props.project.project_url ? <p className={s.addLinks}><a target="_blank" rel="external" href={this.props.project.project_url}>Project Link</a></p> : null}
+            <p className={s.addLinks}><a target="_blank" rel="external" href={project.video_presentation_url}>Video Presentation</a></p>
+            {project.project_url ? <p className={s.addLinks}><a target="_blank" rel="external" href={project.project_url}>Project Link</a></p> : null}
 
           </div>
 
