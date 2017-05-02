3ed31bd625d199bd092209c58ec4e889683483ac
Yining Shi <ys2189@nyu.edu>
2017-05-02 14:34:15 -0400

    update about info

--- src/AboutPage/AboutCreditsPage.js
+++ src/AboutPage/AboutCreditsPage.js
@@ -4,28 +4,29 @@ import {html} from './credit.md'
 export const AboutCreditsPage = () => {
   return (
     <div>
-      <p>PROJECT REVIEWERS</p>
+      <p>Coming soon!</p>
+      {/*<p>PROJECT REVIEWERS</p>*/}
 
 
-      <p>SPEAKERS</p>
+      {/*<p>SPEAKERS</p>*/}
 
 
-      <p>THESIS ARCHIVE 2016 WEBSITE</p>
+      {/*<p>THESIS ARCHIVE 2016 WEBSITE</p>*/}
 
 
-      <p>THESIS ADVISORS</p>
+      {/*<p>THESIS ADVISORS</p>*/}
 
 
-      <p>FULL-FACULTY<br/>
-        Dan O’Sullivan(Chair), Benedetta Piantella, Clay Shirky, Dan Shiffman,
-        Daniel Rozin, Gabe Barcia-Colombo, Katherine Dillon, Luke DuBois,
-        Marianne Petit, Marina Zurkow, Mimi Yin, Mitchell Joachim
-        Nancy Hechinger, Shawn Van Every, Tom Igoe</p>
+      {/*<p>FULL-FACULTY<br/>*/}
+        {/*Dan O’Sullivan(Chair), Benedetta Piantella, Clay Shirky, Dan Shiffman,*/}
+        {/*Daniel Rozin, Gabe Barcia-Colombo, Katherine Dillon, Luke DuBois,*/}
+        {/*Marianne Petit, Marina Zurkow, Mimi Yin, Mitchell Joachim*/}
+        {/*Nancy Hechinger, Shawn Van Every, Tom Igoe</p>*/}
 
-      <p>STAFF AND ADMIN<br/>
-        George Agudow, Administrative Director, Amy Lui, Ben Light,
-        Dante DelGiacco, John Duane, Marlon Evans, Midori Yasuda,
-        Rob Ryan, Yen-An Chen</p>
+      {/*<p>STAFF AND ADMIN<br/>*/}
+        {/*George Agudow, Administrative Director, Amy Lui, Ben Light,*/}
+        {/*Dante DelGiacco, John Duane, Marlon Evans, Midori Yasuda,*/}
+        {/*Rob Ryan, Yen-An Chen</p>*/}
     </div>
   )
 }
