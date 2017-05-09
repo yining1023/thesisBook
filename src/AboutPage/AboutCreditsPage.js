56b04b9e98ac8673de41510349e1a3a14c70477e
Yining Shi <ys2189@nyu.edu>
2017-05-09 01:49:54 -0400

    updated about page content

--- src/AboutPage/AboutCreditsPage.js
+++ src/AboutPage/AboutCreditsPage.js
@@ -4,36 +4,162 @@ import s from './styles.css'
 
 export const AboutCreditsPage = () => {
   return (
-    <div>
-      <p>THESIS ARCHIVE 2017 WEBSITE</p>
-      <p>Katherine Dillon, Team Leader</p>
-      <p>Nancy Hechinger, Project Consultant and Poet</p>
-      <p>Rebecca S Lieberman, Designer</p>
-      <p><a href="http://www.1023.io" target="_blank">Yining Shi</a>, Front-end Developer</p>
-      <p>Yen Chen, Back-end Developer</p>
-      <p><a href="http://www.matamala.info" target="_blank">Alejandro Matamala</a>, Production Associate</p>
-      {/*<p>PROJECT REVIEWERS</p>*/}
+    <div className={s.credits}>
+      <div>
+        <p><b>THESIS ADVISORS:</b><br/>
+        Nancy Hechinger (Area Head)<br/>
+        Gabe Barcia-Colombo<br/>
+        Katherine Dillon<br/>
+        Andrew Lazarow<br/>
+        Robin Reid<br/>
+        Kathleen Wilson<br/>
+        Benedetta Piantella<br/>
+        </p>
+
+        <br/>
+
+        <p><b>FULL-FACULTY:</b><br/>
+        Dan O’Sullivan (Chair)<br/>
+        Benedetta Piantella<br/>
+        Dan Shiffman<br/>
+        Daniel Rozin<br/>
+        Gabe Barcia-Colombo<br/>
+        Katherine Dillon<br/>
+        Allison Parrish<br/>
+        Luke DuBois<br/>
+        Marina Zurkow<br/>
+        Mimi Yin<br/>
+        Mitchell Joachim<br/>
+        Nancy Hechinger<br/>
+        Shawn Van Every<br/>
+        Tom Igoe<br/></p>
+
+        <br/>
+
+        <p><b>RESEARCH RESIDENTS:</b><br/>
+        Tanya Campbell<br/>
+        Rebecca Lieberman<br/>
+        Chang (Cici) Liu<br/>
+        Rubin Huang<br/>
+        Joseph Mango<br/>
+        Justin Peake<br/>
+        Craig Pickard<br/>
+        Matt Romein<br/>
+        Yining Shi<br/>
+        Abhishek Singh<br/>
+        Kat Sullivan<br/>
+        Jingwen Zhu<br/></p>
+
+        <br/>
+
+        <p><b>STAFF AND ADMIN:</b><br/>
+        George Agudow<br/>
+        Anna Gallagher<br/>
+        Dante DelGiacco<br/>
+        John Duane<br/>
+        Marlon Evans<br/>
+        Midori Yasuda<br/>
+        Rob Ryan<br/>
+        Yen-An Chen<br/>
+        Ben Light<br/></p>
+
+        <br/>
 
 
-      {/*<p>SPEAKERS</p>*/}
+        <p><b>SPECIAL THANKS</b><br/>
+          Eric Rosenthal (Godfather of Thesis)<br/>
+          Margaret Smith (NYU Physical Sciences LIbrarian)<br/>
+          Adaora Udoji (Presentation Guide extraordinaire)<br/>
+          Anna Gallagher (Amazing!)<br/></p>
 
+        <br/>
 
-      {/*<p>THESIS ARCHIVE 2016 WEBSITE</p>*/}
+        <p>Special Thanks to the First Years and next year’s <br/> First Years for their comments at the Quick & Dirty Thesis Show.</p>
 
+      </div>
+      <div>
+        <p><b>THESIS ARCHIVE 2017 WEBSITE</b><br/>
+          Katherine Dillon, Team Leader<br/>
+          Nancy Hechinger, Project Consultant and Poet<br/>
+          <a href="http://www.1023.io" target="_blank">Yining Shi</a>, Front-end Developer<br/>
+          Rebecca S Lieberman, Designer<br/>
+          Yen Chen, Back-end Developer<br/>
+          <a href="http://www.matamala.info" target="_blank">Alejandro Matamala</a>, Production Associate<br/></p>
 
-      {/*<p>THESIS ADVISORS</p>*/}
+        <br/>
 
-
-      {/*<p>FULL-FACULTY<br/>*/}
-        {/*Dan O’Sullivan(Chair), Benedetta Piantella, Clay Shirky, Dan Shiffman,*/}
-        {/*Daniel Rozin, Gabe Barcia-Colombo, Katherine Dillon, Luke DuBois,*/}
-        {/*Marianne Petit, Marina Zurkow, Mimi Yin, Mitchell Joachim*/}
-        {/*Nancy Hechinger, Shawn Van Every, Tom Igoe</p>*/}
-
-      {/*<p>STAFF AND ADMIN<br/>*/}
-        {/*George Agudow, Administrative Director, Amy Lui, Ben Light,*/}
-        {/*Dante DelGiacco, John Duane, Marlon Evans, Midori Yasuda,*/}
-        {/*Rob Ryan, Yen-An Chen</p>*/}
+        <p><b>Thanks to all who reviewed our students’ work and gave talks on Thesis Tuesdays:</b><br/>
+        Adam Quinn<br/>
+        Alex Kaufman<br/>
+        Alex Vlack<br/>
+        Alon Benari<br/>
+        Alon Chitayat<br/>
+        Amy Khoshbin<br/>
+        Andrea Wolf<br/>
+        Andy Sigler<br/>
+        Anthony Dong<br/>
+        Ben Light<br/>
+        Brett Peterson<br/>
+        Bruce Lincoln<br/>
+        Caitlyn Berrigan<br/>
+        Calli Higgins<br/>
+        Caroline Fraser<br/>
+        Che-Wei Wang<br/>
+        Chris Kairalla<br/>
+        Claire Kearney-Volpe<br/>
+        Danne Woo<br/>
+        David Lobser<br/>
+        Despina Papadopoulos<br/>
+        Diana Freed<br/>
+        Donna Masini<br/>
+        Edwin Reed-Sanchez<br/>
+        Gilad Lotan<br/>
+        Heather Velez<br/>
+        Jack Pascarosa<br/>
+        James George<br/>
+        James Richardson<br/>
+        Jeff Feddersen<br/>
+        Jeremy Scott-Diamond<br/>
+        Joanie Thompson<br/>
+        Jon Wasserman<br/>
+        Josh Knowles<br/>
+        Kate Tibbetts<br/>
+        Kenyatta Cheese<br/>
+        Kristina Budelis<br/>
+        Laura Chen<br/>
+        Lilly Preston<br/>
+        Lily Szajnberg<br/>
+        Luke DuBois<br/>
+        Michael Cosaboom<br/>
+        Mike Dory<br/>
+        Mimo Onuoha<br/>
+        Nancy Nowacek<br/>
+        Omer Shapira<br/>
+        Patricia Zablah<br/>
+        Patrick Hebron<br/>
+        Pravin Sathe<br/>
+        Rosalie Yu<br/>
+        Rune Madsen<br/>
+        Sally Rudoy<br/>
+        Sam Lavigne<br/>
+        Sanniti Pimpley<br/>
+        Sarah Rothberg<br/>
+        Sehyun Kim<br/>
+        Sergio Mora-Diaz<br/>
+        Shari Berman<br/>
+        Sharon de la Cruz<br/>
+        Su Hyun Kim<br/>
+        Summer Bedard<br/>
+        Taeyoon  Choi<br/>
+        Taylor Levy<br/>
+        TK Broderick<br/>
+        Tom Hennes<br/>
+        Vikram Tank<br/>
+        Winslow Porter<br/>
+        Yotam Mann<br/>
+          Ziv Schneider<br/>
+        </p>
+      </div>
     </div>
   )
 }
